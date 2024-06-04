db3fbb179e35e1fdac167f016e9485b013428f52
Polina Holotsvan <swan.polina00@gmail.com>
2024-06-04 22:19:15 +0300

    Add code encryption into main.py

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
+from algo.aes import encrypt
 from src.scheme import User
 import time
 
+from algo.shrek import exit_handler
+import atexit
 
-vika = User('vika')
-#margo = User('margo')
-vika.connect()
-#margo.connect()
-time.sleep(15)
-vika.send_message('hi')
-time.sleep(15)
-vika.disconnect()
-#time.sleep(4)
-#margo.disconnect()
+atexit.register(exit_handler)
+
+"""change user name"""
+
+your_user = User('your_user')
+your_user.connect()
+time.sleep(30)
+message = "hi"
+
+enc_message = encrypt(message, str(your_user.private_key))
+your_user.send_message(str(enc_message))
+time.sleep(30)
+your_user.disconnect()
